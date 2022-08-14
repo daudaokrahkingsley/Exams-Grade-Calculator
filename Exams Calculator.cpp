2cfc6d1ed5c1ee127530403b3ed7746af34d0eb4
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2022-08-14 15:30:13 +0000

    Add files via upload

---
+++ Exams Calculator.cpp
@@ -0,0 +1,294 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+	{
+	
+		float mark,mark1,mark2,mark3,mark4,mark5,mark6,mark7;
+		
+		float average, total;
+		
+		string age, name,hall;
+		
+		float numbers;
+		int credit_hours,gpa, gp;
+		credit_hours= 3; 
+		float cgpa1=4.0,cgpa2=3.5,cgpa3=3.0,cgpa4=2.5,cgpa5=2.0, cgpa6=1.5,cgpa7=1.0,cgpa8=0.5;
+		
+	
+		
+		
+	
+		
+		
+			
+	   cout <<"1.COMMUNICATIVE SKILLS : ";
+	   cin>>mark1;
+	   	
+		
+	    
+	     if(mark1>=80 && mark1<=100){
+	    	cout<<"GRADE REMARK(GD) =A"<<endl;
+	    	
+		}else if(mark1>=75 && mark1<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark1>=70 && mark1<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark1>=65 && mark1<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark1>=60 && mark1<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark1>=55 && mark1<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark1>=50 && mark1<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+		
+	   
+	   		if (mark1>=80 && mark<=100){
+	   			cout<<"CGPA = 4"<<endl;
+	   			cout<<"GP = " <<credit_hours*cgpa1<<endl;
+			   }else
+			     cout<<"9"<<endl;
+
+	   
+	   
+	   
+	   
+	   cout<<"2.AFRICAN STUDIES : ";
+	   cin>>mark2;
+	   
+		 if(mark2<1){
+			cout<<"ERROR"<<endl;	
+		
+	    
+	    }else if(mark2>=80 && mark2<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark2>=75 && mark2<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark2>=70 && mark2<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark2>=65 && mark2<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark2>=60 && mark2<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark2>=55 && mark2<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark2>=50 && mark2<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark2<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	
+		
+		
+		
+		
+	   cout<<"3.MATHEMATICS FOR COMPUTING :";
+	   cin>>mark3;
+	
+	    if(mark3<1){
+			cout<<"ERROR"<<endl;	
+		
+	    
+	    }else if(mark3>=80 && mark3<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark3>=75 && mark3<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark3>=70 && mark1<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark3>=65 && mark3<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark3>=60 && mark3<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark3>=55 && mark3<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark3>=50 && mark3<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark3<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	
+
+	   
+	   cout<<"4.INTRODUCTION TO COMPUTING : ";
+	   cin>>mark4;
+	   
+	    if(mark4<1){
+			cout<<"GRADE REMARK(GD) = ERROR"<<endl;	
+		
+	    
+	    }else if(mark4>=80 && mark4<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark4>=75 && mark4<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark4>=70 && mark4<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark4>=65 && mark4<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark4>=60 && mark4<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark4>=55 && mark4<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark4>=50 && mark4<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark4<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	
+	   
+	   
+	   cout<<"5.INTRODUCTION TO MANAGEMENT :";		
+ 	   cin>>mark5;
+ 	   
+ 	    if(mark5<1){
+			cout<<"ERROR"<<endl;	
+		
+	    }else if(mark5>=80 && mark5<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark5>=75 && mark5<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark5>=70 && mark5<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark5>=65 && mark5<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark5>=60 && mark5<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark5>=55 && mark5<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark5>=50 && mark5<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark5<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	
+	 
+	    cout<<"6.SOFTWARE SUITE :";
+ 	     cin>>mark6;
+ 	
+	 	 if(mark6<1){
+			cout<<"ERROR"<<endl;	
+		
+	    
+	    }else if(mark6>=80 && mark6<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark6>=75 && mark6<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark6>=70 && mark6<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark6>=65 && mark6<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark6>=60 && mark6<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark6>=55 && mark6<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark6>=50 && mark6<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark6<50){
+			cout<<"GRADE REMARK(GD) = E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	
+	 
+	    
+ 	   cout<<"7.IMFORMATION LITERACY :";
+ 	   cin>>mark7;
+ 	 
+ 		 if(mark1<7){
+			cout<<"ERROR"<<endl;	
+		
+	    
+	    }else if(mark7>=80 && mark7<=100){
+	    	cout<<"GRADE REMARK(GD) = A"<<endl;
+	    	
+		}else if(mark7>=75 && mark7<=79){
+			cout <<"GRADE REMARK(GD) = B+"<<endl;
+			
+		}else if(mark7>=70 && mark7<=74){
+			cout <<"GRADE REMARK(GD) = B"<<endl;
+			
+		}else if(mark7>=65 && mark7<=69){
+			cout<<"GRADE REMARK(GD) = C+"<<endl;
+			
+		}else if(mark7>=60 && mark7<=64){
+			cout<<"GRADE REMARK(GD) = C"<<endl;
+		
+		}else if(mark7>=55 && mark7<=59 ){
+			cout<<"GRADE REMARK(GD) = D+"<<endl;
+			
+		}else if(mark7>=50 && mark7<=54){
+			cout<<"GRADE REMARK(GD) = D"<<endl;
+			
+		}else if(mark7<50){
+			cout<<"GRADE REMARK(GD)E"<<endl;
+		}else
+		 cout<<"ERROR OCCURED"<<endl;
+	  
+ 	 total=mark+mark1+mark2+mark3+mark4+mark5+mark6+mark7;
+ 	  cout<<"TOTAL MARK:"<<total<<endl;
+
+     
+    average = total/7;
+    cout << "AVERAGE MARK : " <<average<<endl;
+		
+	
+		
+		
+	return 0;
+		
+	}
+	
+	
+	
+	
+
